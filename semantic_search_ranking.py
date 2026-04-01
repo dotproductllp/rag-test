ab4229cc4cdb3461e4cb3490121274abcc8ae32d
harigovind-dot-a <harigovind@dotproduct.in>
2026-04-01 17:19:15 +0530

    semanic search with score.

---
+++ semantic_search_ranking.py
@@ -0,0 +1,41 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+load_dotenv()
+
+class SemanticSearchEngine:
+    def __init__(self):
+        self.index_name = os.getenv("INDEX_NAME")
+        self.top_k = 7
+
+        self.embeddings = OpenAIEmbeddings(
+            openai_api_key=os.getenv("OPENAI_API_KEY"),
+            model="text-embedding-3-small"
+        )
+
+        self.vectorstore = PineconeVectorStore.from_existing_index(
+            index_name=self.index_name,
+            embedding=self.embeddings
+        )
+        
+    def semantic_search_with_score(self, query):
+        results = self.vectorstore.similarity_search_with_score(
+            query,
+            k=self.top_k
+        )
+        return results
+
+if __name__ == "__main__":
+    engine = SemanticSearchEngine()
+
+    while True:
+        user_query = input("Enter text: ")
+
+        results = engine.semantic_search_with_score(user_query)
+
+        print(f"\nTop {len(results)} results:\n")
+        for i, (doc, score) in enumerate(results, start=1):
+            page = doc.metadata.get("page")
+            snippet = doc.page_content[:900]
+            print(f"{i}-> Score: {score:.4f} | Page: {page} :{snippet}\n")
